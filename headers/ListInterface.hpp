f6ddac14a6270fe88cce37a90e59eb5a5d3a2f9b
Prokhor <len0xx@icloud.com>
2021-04-06 22:41:55 +0500

    Minor fixes

--- headers/ListInterface.hpp
+++ headers/ListInterface.hpp
@@ -17,21 +17,21 @@ public:
     virtual void Append(K val) = 0;
 
     // Append new element to the list with the Node
-    virtual void Append(DS::Node<K>* newNode) = 0;
+    virtual void Append(DS::Node<K> *newNode) = 0;
 
     // Duplicate the Node
-    virtual void Duplicate(DS::Node<K>* newNode) = 0;
+    virtual void Duplicate(DS::Node<K> *newNode) = 0;
 
     // Remove the Node
-    virtual DS::Node<K>* Remove(DS::Node<K>* entry) = 0;
+    virtual DS::Node<K> *Remove(DS::Node<K> *entry) = 0;
 
     // Get the length of the list
     virtual size_t Length(void) const noexcept = 0;
 
     // Find a first Node with the given value
-    virtual DS::Node<K>* Get(K val) const = 0;
+    virtual DS::Node<K> *Get(K val) const = 0;
 
-    virtual DS::Node<K>* GetHead() const noexcept = 0;
+    virtual DS::Node<K> *GetHead() const noexcept = 0;
 
     // Remove all the elements from the list
     virtual void Clear(void) = 0;
